ed7756a7c9d2c2fa12f9af1c00674c15b9fddc0a
GiovaneLM <giovanelimeiramartins@gmail.com>
2024-12-13 10:12:46 -0300

    header e footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                
+            </div>
+        </footer>
+    );
+};
+export default Footer;
